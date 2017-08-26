3eaa6a31cd6a1930d87e652a23abe910493d1cd0
Scorpio <scorpio@Scorps-iMac.local>
2017-08-26 16:28:35 +0300

    rest api amends

--- app/models/MiLidaCommon.php
+++ app/models/MiLidaCommon.php
@@ -9,11 +9,10 @@ class MiLidaCommon extends \Phalcon\Mvc\Model {
      $this->db=$this->getDi()->getShared('db');
   }
 
-	public function getUserInfo($uid)
+	public function getUserInfo($token)
    {
       //print_r(\Phalcon\Di::getDefault()->getShared('db')); // This is the ugly way to grab the connection.
-
-      $result=$this->db->query("SELECT * FROM users"); // Working now
+      $result=$this->db->query("SELECT * FROM active_user_sessions where access_token = :atoken",Db::FETCH_ASSOC,$token); 
       return $result->fetchAll();
    }
 }
