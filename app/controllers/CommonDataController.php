dfb9b9850e5b25dbbecccc563064c7a2f49512d7
Scorpio <scorpio@Scorps-iMac.local>
2017-08-27 00:48:43 +0300

    rest api amends

--- app/controllers/CommonDataController.php
+++ app/controllers/CommonDataController.php
@@ -3,7 +3,7 @@ use Phalcon\Http\Response;
 namespace Controllers;
 
 class CommonDataController extends \Phalcon\Mvc\Controller {
-
+//http://172.16.130.180/restlida/user-data?token=20WIh7QKUt8U0sJBOMTAYmRy0ZNFwkeQn6LPSeeD
 	public function getCurrentUserInformation() {
 			 $MiLidaCommonModel = new \Models\MiLidaCommon();
 			 $UserInfo = $MiLidaCommonModel->getUserInfo($this->request->get("token"));
@@ -19,7 +19,7 @@ class CommonDataController extends \Phalcon\Mvc\Controller {
 				 $UserInfo = $MiLidaCommonModel->getUserInfo($this->request->get("token"));
 				 if (isset($UserInfo['uid'])&&$UserInfo['uid']>1&&$UserInfo['uid']!=3){
 					$data=$this->request->getJsonRawBody();
-				 	$MiLidaCommonModel->createGropup($data);
+				 	$MiLidaCommonModel->createGropup($data,$UserInfo['uid']);
 				 	$res='ok';
 			   }
 			 }
