6a25b231e37958dd8d932b8ffbce044d4ad614a1
Scorpio <scorpio@Scorps-iMac.local>
2017-08-26 18:05:40 +0300

    rest api amends

--- app/models/MiLidaCommon.php
+++ app/models/MiLidaCommon.php
@@ -11,9 +11,9 @@ class MiLidaCommon extends \Phalcon\Mvc\Model {
 	public function getUserInfo($token)
    {
       //print_r(\Phalcon\Di::getDefault()->getShared('db')); // This is the ugly way to grab the connection.
-			$this->db->query("SET NAMES 'utf8'");
+			/*$this->db->query("SET NAMES 'utf8'");
 			$this->db->query("SET CHARACTER SET 'utf8'");
-			$this->db->query("SET SESSION collation_connection = 'utf8_general_ci'");
+			$this->db->query("SET SESSION collation_connection = 'utf8_general_ci'");*/
       $result=$this->db->fetchOne("SELECT * FROM active_user_sessions where access_token = :atoken",\Phalcon\Db::FETCH_ASSOC,['atoken'=>$token]);
       return $result;
    }
