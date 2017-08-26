0691a276821c0e1f672f688b5252f3379a2d1fe2
Scorpio <scorpio@Scorps-iMac.local>
2017-08-26 15:14:24 +0300

    rest api amends

--- app/controllers/CommonDataController.php
+++ app/controllers/CommonDataController.php
@@ -7,7 +7,7 @@ class CommonDataController extends \Phalcon\Mvc\Controller {
 
 
 	public static function getCurrentUserInformation() {
-			 $MiLidaCommonModel = new Models\MiLidaCommon();
+			 $MiLidaCommonModel = new \Models\MiLidaCommon();
 			 $UserInfo = $MiLidaCommonModel->getUserInfo(2);
 			 $Response = new \Phalcon\Http\Response();
 			// $Response->setJsonContent(['ppppsdf','sdfsdfsdf','111111sdf']);
