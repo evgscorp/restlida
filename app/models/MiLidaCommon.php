cbbea9769f8bfa0ad7a8f81694acb9d5c6f4ee66
git <git@scorp.com>
2017-08-31 09:58:30 +0300

    fixes

--- app/models/MiLidaCommon.php
+++ app/models/MiLidaCommon.php
@@ -10,15 +10,15 @@ class MiLidaCommon extends \Phalcon\Mvc\Model {
 
 	public function getShiftProductionInfo($gid)
    {
-		 $sql_packages="SELECT count(*) cnt FROM packages_info where operation_id = :operation_id and group_id =:group_id";
-		 $sql_pallets="SELECT count(d.pallet_id) cnt FROM (SELECT pallet_id FROM packages_info where operation_id = :operation_id and group_id =:group_id and pallet_id >0 group by pallet_id ) d";
-		 $sql_first_package="SELECT timestmp FROM packages_info where operation_id = :operation_id and group_id =:group_id  order by timestmp desc limit 1";
+		 $sql_packages="SELECT count(*) cnt FROM packages_info where group_id =:group_id";
+		 $sql_pallets="SELECT count(d.pallet_id) cnt FROM (SELECT pallet_id FROM packages_info where  group_id =:group_id and pallet_id >0 group by pallet_id ) d";
+		 $sql_first_package="SELECT timestmp FROM packages_info where  group_id =:group_id  order by timestmp desc limit 1";
 		 $this->utf8init();
-		 $result['packages_produced']=$this->db->fetchColumn($sql_packages,['operation_id'=>17,'group_id'=>$gid],'cnt');
-		 $result['packages_passed']=$this->db->fetchColumn($sql_packages,['operation_id'=>2,'group_id'=>$gid],'cnt');
-		 $result['pallets_produced']=$this->db->fetchColumn($sql_pallets,['operation_id'=>17,'group_id'=>$gid],'cnt');
-     $result['pallets_passed']=$this->db->fetchColumn($sql_pallets,['operation_id'=>2,'group_id'=>$gid],'cnt');
-		 $result['first_package']=$this->db->fetchColumn($sql_first_package,['operation_id'=>2,'group_id'=>$gid],'timestmp');
+		 $result['packages_produced']=$this->db->fetchColumn($sql_packages,['group_id'=>$gid],'cnt');
+		 $result['packages_passed']=$this->db->fetchColumn($sql_packages,['operation_id'=>4,'group_id'=>$gid],'cnt');
+		 $result['pallets_produced']=$this->db->fetchColumn($sql_pallets,['group_id'=>$gid],'cnt');
+     $result['pallets_passed']=$this->db->fetchColumn($sql_pallets,['operation_id'=>4,'group_id'=>$gid],'cnt');
+		 $result['first_package']=$this->db->fetchColumn($sql_first_package,['group_id'=>$gid],'timestmp');
      return $result;
    }
 
@@ -29,6 +29,9 @@ class MiLidaCommon extends \Phalcon\Mvc\Model {
       //print_r(\Phalcon\Di::getDefault()->getShared('db')); // This is the ugly way to grab the connection.
 			$this->utf8init();
 			$result=$this->db->fetchOne("SELECT * FROM active_user_sessions where access_token = :atoken",\Phalcon\Db::FETCH_ASSOC,['atoken'=>$token]);
+			$result=$this->db->fetchOne("SELECT * FROM active_user_sessions where access_token = :atoken",\Phalcon\Db::FETCH_ASSOC,['atoken'=>$token]);
+
+
       return $result;
    }
 
