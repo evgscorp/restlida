a0ca3b8e7fd763957444b8a8980cc8fc6c20168b
git <git@scorp.com>
2017-10-19 16:52:33 +0300

    r upd

--- app/models/MiLidaCommon.php
+++ app/models/MiLidaCommon.php
@@ -10,25 +10,28 @@ class MiLidaCommon extends \Phalcon\Mvc\Model {
 
  public function getSeriesPackages($search){
 	 $sql_search_series="SELECT * FROM milida.series where series_num=:snum";
-	 $sql_info_by_series="SELECT l.*, p.*, g.*, s.*, pl.* FROM milida.packages p
+	 $sql_info_by_series="SELECT sh.*, l.*, p.*, g.*, s.*, pl.* FROM milida.packages p
 												LEFT OUTER JOIN groups g on g.group_id=p.group_id
 												LEFT OUTER JOIN series s on p.series_id = s.series_id
 												LEFT OUTER JOIN preloaded_labels l on p.label_id = l.label_id
 												LEFT OUTER JOIN pallets pl on p.pallet_id = pl.pallet_id
+												LEFT OUTER JOIN shifts sh on sh.shift_id=g.shift_id
 												where s.series_num=:sid  order by idpackage LIMIT 1";
-	$sql_info_by_package="SELECT l.*, p.*, g.*, s.*, pl.* FROM milida.packages p
+	$sql_info_by_package="SELECT sh.*, l.*, p.*, g.*, s.*, pl.* FROM milida.packages p
 												LEFT OUTER JOIN groups g on g.group_id=p.group_id
 												LEFT OUTER JOIN series s on p.series_id = s.series_id
 												LEFT OUTER JOIN preloaded_labels l on p.label_id = l.label_id
 												LEFT OUTER JOIN pallets pl on p.pallet_id = pl.pallet_id
+												LEFT OUTER JOIN shifts sh on sh.shift_id=g.shift_id
 												where l.UUID=:uuid order by idpackage LIMIT 1";
- $sql_packages="SELECT l.*, p.*, g.*, s.*, pl.* FROM milida.packages p
+ $sql_packages="SELECT sh.*, l.*, p.*, g.*, s.*, pl.* FROM milida.packages p
 												LEFT OUTER JOIN groups g on g.group_id=p.group_id
 												LEFT OUTER JOIN series s on p.series_id = s.series_id
 												LEFT OUTER JOIN preloaded_labels l on p.label_id = l.label_id
 												LEFT OUTER JOIN pallets pl on p.pallet_id = pl.pallet_id
+												LEFT OUTER JOIN shifts sh on sh.shift_id=g.shift_id
 												where s.series_num=:sid  order by idpackage ";
-
+ $this->utf8init();
  $result['series']=$this->db->fetchOne($sql_info_by_series,\Phalcon\Db::FETCH_ASSOC,['sid'=>$search]);
  if (!isset($result['series']['idpackage'])||$result['series']['idpackage']<1){
 	 $result['series']=$this->db->fetchOne($sql_info_by_package,\Phalcon\Db::FETCH_ASSOC,['uuid'=>$search]);
