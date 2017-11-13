245b0eb604bde54cd35ca9ba752c8318ef275f59
Scorpio <scorpio@Scorps-iMac.local>
2017-11-13 23:52:21 +0300

    charts api upd

--- app/models/MiLidaCommon.php
+++ app/models/MiLidaCommon.php
@@ -125,12 +125,20 @@ return $result;
 						LEFT OUTER JOIN users u on u.uid=s.uid where s.cd=s.shd order by s.shift_id limit 1";
 			$qoptions=['timestmp'=>intval($timestmp)];
 			if ($action=="prev"){
-			 $sql="SELECT s.*, u.firstname, u.lastname FROM storage_shifts s where shift_id < :shid  and shift_id in (select shift_id from groups group by shift_id )
-			 LEFT OUTER JOIN users u on u.uid=s.uid order by shift_id desc limit 1";
+			 $sql="SELECT s.*, u.firstname, u.lastname FROM storage_shifts s  LEFT OUTER JOIN users u on u.uid=s.uid where shift_id < :shid  and shift_id in (select r.shift_id  from (
+						select sshid shift_id from pallets where sshid is not null
+						union
+						select dsshid shift_id from pallets where dsshid is not null ) r
+						group by r.shift_id
+						)  order by shift_id desc limit 1";
 			 $qoptions=['shid'=>intval($shid)];
 		 } else if ($action=="next"){
-			$sql="SELECT s.*, u.firstname, u.lastname FROM storage_shifts s  where shift_id > :shid  and shift_id in (select shift_id from groups group by shift_id )
-			 LEFT OUTER JOIN users u on u.uid=s.uid order by shift_id desc limit 1";
+			$sql="SELECT s.*, u.firstname, u.lastname FROM storage_shifts s  LEFT OUTER JOIN users u on u.uid=s.uid where shift_id > :shid  and shift_id in (select r.shift_id  from (
+					 select sshid shift_id from pallets where sshid is not null
+					 union
+					 select dsshid shift_id from pallets where dsshid is not null ) r
+					 group by r.shift_id
+					 )  order by shift_id limit 1";
 			$qoptions=['shid'=>intval($shid)];
 		 }
 
