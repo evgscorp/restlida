e5a43f2a841d45c6e0a9859d8f3e0f5f1f555819
git <git@scorp.com>
2017-11-15 12:19:06 +0300

    storage rep uptd

--- app/models/MiLidaCommon.php
+++ app/models/MiLidaCommon.php
@@ -204,14 +204,14 @@ order by creation_time desc";
             $res[$row[$gkey]][]=['name'=>$this->getProductShortName($row[$nkey]),'value'=>$row[$vkey]];
         }
         foreach ($res as $key=>$value) {
-					/*	if (count($value)==1){
+						if (count($value)==1){
 							if ($value[0]['name']==$this->getProductShortName('1'))
-								$value[1]=['name'=>$this->getProductShortName('10'),'value'=>'1'];
-							else $value[1]=['name'=>$this->getProductShortName('1'),'value'=>'1'];
+								$value[1]=['name'=>$this->getProductShortName('10'),'value'=>'0'];
+							else $value[1]=['name'=>$this->getProductShortName('1'),'value'=>'0'];
 						} elseif(count($value)<1){
-								$value[1]=['name'=>$this->getProductShortName('10'),'value'=>'1'];
-								$value[1]=['name'=>$this->getProductShortName('1'),'value'=>'1'];
-						}*/
+								$value[0]=['name'=>$this->getProductShortName('10'),'value'=>'0'];
+								$value[0]=['name'=>$this->getProductShortName('1'),'value'=>'0'];
+						}
 
 					  $result[]=[
             'name'=>$key,
