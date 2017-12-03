e2ab9811e01a55858fb2a17c6ff0ad674e91e9a0
Scorpio <scorpio@Scorps-iMac.local>
2017-12-04 00:41:46 +0300

    job cache api uptd

--- app/models/MiLidaCommon.php
+++ app/models/MiLidaCommon.php
@@ -74,6 +74,7 @@ order by creation_time desc";
                        LEFT OUTER JOIN groups g on g.group_id=p.group_id";*/
 
         $shift_suffix=" AND pa.sshid=:sshid";
+        $shift_delivery_suffix=" AND pa.dsshid=:sshid";
 
         $sql_total="SELECT count(*) cnt FROM milida.packages p
 							LEFT OUTER JOIN preloaded_labels pl on pl.label_id=p.label_id
@@ -81,6 +82,8 @@ order by creation_time desc";
 							LEFT OUTER JOIN groups g on g.group_id=p.group_id WHERE pl.operation_id IN (:operation_id,:operation_id2)";
 
         $sql_shift_total=$sql_total.$shift_suffix;
+        $sql_shift_delivery_total=$sql_total.$shift_delivery_suffix;
+
 
         $sql_weight_total="SELECT sum(g.weight)/1000 weight FROM milida.packages p
 							LEFT OUTER JOIN preloaded_labels pl on pl.label_id=p.label_id
@@ -88,6 +91,7 @@ order by creation_time desc";
 							LEFT OUTER JOIN groups g on g.group_id=p.group_id WHERE pl.operation_id IN (:operation_id,:operation_id2)";
 
         $sql_shift_weight_total=$sql_weight_total.$shift_suffix;
+        $sql_shift_delivery_weight_total=$sql_weight_total.$shift_delivery_suffix;
 
 
         $sql_series="SELECT DISTINCT s.series_num FROM milida.packages p
@@ -98,6 +102,8 @@ order by creation_time desc";
 								WHERE s.series_num is not null AND pl.operation_id IN (:operation_id,:operation_id2)";
 
         $sql_shift_series=$sql_series.$shift_suffix;
+        $sql_shift_delivery_series=$sql_series.$shift_delivery_suffix;
+
 
         $sql_shift_chart="SELECT count(t.idpackage) cnt, t.product_type, t.h from (
 								SELECT p.idpackage, g.product_type, DATE_FORMAT(pa.storage_time, '%d.%m  %H ч.' ) h  FROM milida.packages p
@@ -165,13 +171,14 @@ order by creation_time desc";
         $result['shift_sql_options']=$qoptions;
         $shid=$result['shift_info']['shift_id'];
         $result['total_packages']=$this->db->fetchColumn($sql_total, ['operation_id'=>105,'operation_id2'=>105], 'cnt');
-        $result['total_shift_packages']=$this->db->fetchColumn($sql_shift_total, ['operation_id'=>105,'operation_id2'=>10,'sshid'=>$shid], 'cnt');
-        $result['total_shift_packages_delivered']=$this->db->fetchColumn($sql_shift_total, ['operation_id'=>10,'operation_id2'=>10,'sshid'=>$shid], 'cnt');
+        $result['total_shift_packages']=$this->db->fetchColumn($sql_shift_total, ['operation_id'=>105,'operation_id2'=>105,'sshid'=>$shid], 'cnt');
+        $result['total_shift_packages_delivered']=$this->db->fetchColumn($sql_shift_delivery_total, ['operation_id'=>10,'operation_id2'=>10,'sshid'=>$shid], 'cnt');
         $result['total_weight']=$this->db->fetchColumn($sql_weight_total, ['operation_id'=>105,'operation_id2'=>105], 'weight');
-        $result['total_shift_weight']=$this->db->fetchColumn($sql_shift_weight_total, ['operation_id'=>105,'operation_id2'=>10,'sshid'=>$shid], 'weight');
-        $result['total_shift_weight_delivered']=$this->db->fetchColumn($sql_shift_weight_total, ['operation_id'=>10,'operation_id2'=>10,'sshid'=>$shid], 'weight');
+        $result['total_shift_weight']=$this->db->fetchColumn($sql_shift_weight_total, ['operation_id'=>105,'operation_id2'=>105,'sshid'=>$shid], 'weight');
+        $result['total_shift_weight_delivered']=$this->db->fetchColumn($sql_shift_delivery_weight_total, ['operation_id'=>10,'operation_id2'=>10,'sshid'=>$shid], 'weight');
         $result['series']=$this->db->fetchAll($sql_series, \Phalcon\Db::FETCH_ASSOC, ['operation_id'=>105,'operation_id2'=>105]);
-        $result['shift_series']=$this->db->fetchAll($sql_shift_series, \Phalcon\Db::FETCH_ASSOC, ['operation_id'=>105,'operation_id2'=>10,'sshid'=>$shid]);
+        $result['shift_series']=$this->db->fetchAll($sql_shift_series, \Phalcon\Db::FETCH_ASSOC, ['operation_id'=>105,'operation_id2'=>105,'sshid'=>$shid]);
+        $result['shift_delivery_series']=$this->db->fetchAll($sql_shift_delivery_series, \Phalcon\Db::FETCH_ASSOC, ['operation_id'=>10,'operation_id2'=>10,'sshid'=>$shid]);
         $result['chart']=$this->db->fetchAll($sql_chart, \Phalcon\Db::FETCH_ASSOC, ['operation_id'=>105]);
         $result['chart']=$this->prepareGroupChart($result['chart'], 'h', 'product_type', 'cnt');
         $result['shift_chart']=$this->db->fetchAll($sql_shift_chart, \Phalcon\Db::FETCH_ASSOC, ['operation_id'=>105,'operation_id2'=>10,'sshid'=>$shid]);
