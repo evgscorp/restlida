cf3dc38b06c1ee07fdef5f075dd888fe7370a6b4
git <git@scorp.com>
2018-02-26 16:14:06 +0300

    series form data

--- app/models/MiLidaCommon.php
+++ app/models/MiLidaCommon.php
@@ -319,7 +319,7 @@ class MiLidaCommon extends \Phalcon\Mvc\Model
              where c.workshop_id=:wid LIMIT 1";
         }
 
-        $sql_pallets="SELECT count(*) cnt , pallet_id from packages where series_id = :sid GROUP BY pallet_id  ORDER BY ISNULL(pallet_id), pallet_id ASC";
+        $sql_pallets="SELECT count(*) cnt , pallet_id from packages where series_id = :sid and pallet_id is not null GROUP BY pallet_id  ORDER BY ISNULL(pallet_id), pallet_id ASC";
         $sql_local_storage_info="SELECT * FROM overview_by_location  where workshop_id=:wid and location_id=:wid limit 50";
         $sql_passed_storages_info="SELECT DISTINCT location_id, location_name FROM overview_by_location  where workshop_id=:wid and location_id <>:lid";
         $sql_external_storages_info="SELECT * FROM overview_by_location  where workshop_id=:wid and location_id=:lid limit 25";
