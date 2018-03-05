f6c58bdd3eb35847b8b174e27125c6c2ed2b5d0b
git <git@scorp.com>
2018-03-05 11:35:22 +0300

    s

--- app/models/MiLidaCommon.php
+++ app/models/MiLidaCommon.php
@@ -546,9 +546,9 @@ class MiLidaCommon extends \Phalcon\Mvc\Model
         } 
 
         $result['report2'][0]=$this->db->fetchAll("CALL report_2(0);", \Phalcon\Db::FETCH_ASSOC, []);
-        $result['report2'][1]=$this->db->fetchAll("CALL report_2(21);", \Phalcon\Db::FETCH_ASSOC, []);
-        $result['report2'][2]=$this->db->fetchAll("CALL report_2(22);", \Phalcon\Db::FETCH_ASSOC, []);
-        $result['report2'][3]=$this->db->fetchAll("CALL report_2(23);", \Phalcon\Db::FETCH_ASSOC, []);
+        //$result['report2'][1]=$this->db->fetchAll("CALL report_2(21);", \Phalcon\Db::FETCH_ASSOC, []);
+        //$result['report2'][2]=$this->db->fetchAll("CALL report_2(22);", \Phalcon\Db::FETCH_ASSOC, []);
+        //$result['report2'][3]=$this->db->fetchAll("CALL report_2(23);", \Phalcon\Db::FETCH_ASSOC, []);
        
        /*
         foreach ($result["storageWorkshops"] as $srow) {
