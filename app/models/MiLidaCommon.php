9ffa756c00843c631a4eecca56d3194eef515f7b
git <git@scorp.com>
2018-03-05 16:32:44 +0300

    sss

--- app/models/MiLidaCommon.php
+++ app/models/MiLidaCommon.php
@@ -25,7 +25,7 @@ class MiLidaCommon extends \Phalcon\Mvc\Model
          $sql="SELECT p.*, s.*, pp.pallet_code, pp.creation_time, ll.*
             	from (SELECT count(*) cnt, pallet_id, mp.location_id  FROM packages mp
             	 where pallet_id is not null and pallet_id in
-                 ( SELECT pallet_id FROM overview_by_location where location_id >:lid and  location_id < :mlid and workshop_id =:wid)
+                 ( SELECT pallet_id FROM overview_by_location where location_id >:lid and  location_id < :mlid )
             	group by pallet_id, mp.location_id ) p
             left outer join series s on s.series_id = (select max(series_id) from packages where pallet_id=p.pallet_id)
             left outer join pallets pp on p.pallet_id=pp.pallet_id
@@ -41,7 +41,7 @@ class MiLidaCommon extends \Phalcon\Mvc\Model
         where pallet_id is not null and pallet_id in ( SELECT pallet_id FROM overview_by_location where location_id >:lid and  location_id < :mlid and workshop_id=:wid )";
 
          $lid=10;
-         $mlid=30;
+         $mlid=20;
         if ($shipment!="0") {$lid=30; $mlid=40;};
         $sql_locations="SELECT * FROM locations where location_id > 20 and location_id < 40";
         $this->utf8init();
