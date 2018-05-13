a04b4b2670f6f717e92c740861bc60ada1f75eab
Scorpio <scorpio@Scorps-iMac.local>
2018-05-14 01:01:51 +0300

    s

--- app/models/MiLidaCommon.php
+++ app/models/MiLidaCommon.php
@@ -871,7 +871,7 @@ class MiLidaCommon extends \Phalcon\Mvc\Model
 
     public function updateShipmentItem($data) {
         //$date = ''.date($data->date_invoice);
-        $date=date("Y-m-d",intval($stdate)).' 10:00:00';
+        $date=date("Y-m-d",intval($data)).' 10:00:00';
         if (isset($data->id)&&($data->id < 0)) {
             $sql="UPDATE `shipments` SET `doc_number`='$data->doc1', `doc_number2`='$data->doc2', 
             `client_name`='$data->client', `vh_number`='$data->car', `driver_name`='$data->driver', `driver_name`='$data->driver', `ship_stmp`='$date'
