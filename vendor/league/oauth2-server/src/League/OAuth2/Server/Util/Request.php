43f60e8e8706628e61f13e3bcbcbfaa8e08f2f2d
evgscorp <evgfree@gmail.com>
2019-03-20 15:29:17 +0300

    fx

--- vendor/league/oauth2-server/src/League/OAuth2/Server/Util/Request.php
+++ vendor/league/oauth2-server/src/League/OAuth2/Server/Util/Request.php
@@ -49,6 +49,12 @@ class Request implements RequestInterface
         return $this->getPropertyValue('get', $index, $default);
     }
 
+    public function options($index = null, $default = null)
+    {
+        return $this->get($index, $default);
+    }
+    
+
     public function post($index = null, $default = null)
     {
         return $this->getPropertyValue('post', $index, $default);
