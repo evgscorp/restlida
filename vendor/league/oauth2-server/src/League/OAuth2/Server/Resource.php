aeb2eefcadadc73443eb9996b75078e0d048c31e
evgscorp <evgfree@gmail.com>
2019-03-20 15:54:37 +0300

    fx

--- vendor/league/oauth2-server/src/League/OAuth2/Server/Resource.php
+++ vendor/league/oauth2-server/src/League/OAuth2/Server/Resource.php
@@ -383,7 +383,7 @@ class Resource
             $accessToken = ($accessToken === 'Bearer') ? '' : $accessToken;
         } elseif ($headersOnly === false) {
             $method = $this->getRequest()->server('REQUEST_METHOD');
-            if ($method=="OPTIONS") $method = "cookie";
+            if ($method=="OPTIONS") $method = "header";
             $accessToken = $this->getRequest()->{$method}($this->tokenKey);
         }
       //  print_r($this->getRequest()->allheaders());
